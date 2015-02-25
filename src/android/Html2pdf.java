1726a249e88948ce58536a7db172d3ff8ba66a00
javierClavei <jruiz@clavei.es>
2015-02-25 13:20:52 +0100

    Update Html2pdf.java

--- src/android/Html2pdf.java
+++ src/android/Html2pdf.java
@@ -33,7 +33,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
-import android.print;
+import android.print.*;
 import android.printservice.PrintJob;
 import android.view.View;
 import android.view.ViewGroup;
