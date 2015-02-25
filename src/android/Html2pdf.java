be541f442d4e7d494dfb0d378add3ae28b44cbbf
javierClavei <jruiz@clavei.es>
2015-02-25 15:51:49 +0100

    Update Html2pdf.java

--- src/android/Html2pdf.java
+++ src/android/Html2pdf.java
@@ -517,6 +517,9 @@ public class Html2pdf extends CordovaPlugin
 	                     CancellationSignal cancellationSignal,
 	                     LayoutResultCallback callback,
 	                     Bundle metadata) {
+	                     	
+	    super.onLayout(oldAttributes, newAttributes, cancellationSignal, callback, metadata);                 	
+	                     	
 	    // Create a new PdfDocument with the requested page attributes
 	    mPdfDocument = new PrintedPdfDocument(this.cordova.getActivity(), newAttributes);
 	
